ec0f6e1be0e54e78d221aa1eeb997a491c311655
Aniket kumar <aniket3pd@gmail.com>
2026-09-21 23:22:08 +0530

    Time: 34 ms (57.4%), Space: 59.2 MB (45.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,75 +1,155 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum < 0) {
+                    left++;
+                }
+                else if (sum > 0) {
+                    right--;
+                }
+                else {
+                    res.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
+
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
+
+
+
+
+
+// // // -----------------------------------------------------------------------------------------------------
+
 // class Solution {
 //     public List<List<Integer>> threeSum(int[] nums) {
+
+//         // This will store all valid triplets
 //         List<List<Integer>> res = new ArrayList<>();
+
+//         // Sorting is important because:
+//         // 1. It allows us to use the two-pointer technique.
+//         // 2. It makes it easy to skip duplicates.
 //         Arrays.sort(nums);
 
-//         for (int i = 0; i < nums.length; i++) {
-//             if (i > 0 && nums[i] == nums[i-1]) {
+//         // We need at least 3 numbers, so i can go only up to nums.length - 3
+//         for (int i = 0; i < nums.length - 2; i++) {
+
+//             // If the current number is the same as the previous number,
+//             // skip it because we already processed this starting number.
+//             //
+//             // Example:
+//             // [-1, -1, 0, 1]
+//             //     ↑
+//             // The second -1 would generate the same triplets.
+//             if (i > 0 && nums[i] == nums[i - 1]) {
 //                 continue;
 //             }
-            
-//             int j = i + 1;
-//             int k = nums.length - 1;
 
-//             while (j < k) {
-//                 int total = nums[i] + nums[j] + nums[k];
+//             // left starts immediately after i
+//             int left = i + 1;
 
-//                 if (total > 0) {
-//                     k--;
-//                 } else if (total < 0) {
-//                     j++;
-//                 } else {
-//                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-//                     j++;
+//             // right starts at the last element
+//             int right = nums.length - 1;
 
-//                     while (nums[j] == nums[j-1] && j < k) {
-//                         j++;
+//             // Keep searching while left and right have not crossed
+//             while (left < right) {
+
+//                 // Try to find:
+//                 // nums[i] + nums[left] + nums[right] == 0
+//                 int sum = nums[i] + nums[left] + nums[right];
+
+//                 // If sum is too small, we need a bigger number.
+//                 // Since the array is sorted, move left forward.
+//                 if (sum < 0) {
+//                     left++;
+//                 }
+
+//                 // If sum is too large, we need a smaller number.
+//                 // Since the array is sorted, move right backward.
+//                 else if (sum > 0) {
+//                     right--;
+//                 }
+
+//                 // We found a valid triplet
+//                 else {
+
+//                     // Store the triplet
+//                     res.add(Arrays.asList(
+//                         nums[i],
+//                         nums[left],
+//                         nums[right]
+//                     ));
+
+//                     // We already used these two values.
+//                     // Move both pointers to search for another pair.
+//                     left++;
+//                     right--;
+
+//                     // Skip duplicate values on the left.
+//                     //
+//                     // Example:
+//                     // [-4, 1, 1, 3, 3]
+//                     //        ↑
+//                     // If the previous left value was also 1,
+//                     // using this 1 would produce the same triplet.
+//                     while (left < right &&
+//                            nums[left] == nums[left - 1]) {
+//                         left++;
+//                     }
+
+//                     // Skip duplicate values on the right.
+//                     //
+//                     // Example:
+//                     // [-4, 1, 1, 3, 3]
+//                     //             ↑
+//                     // If the previous right value was also 3,
+//                     // using this 3 would produce the same triplet.
+//                     while (left < right &&
+//                            nums[right] == nums[right + 1]) {
+//                         right--;
 //                     }
 //                 }
 //             }
 //         }
-//         return res;        
+
+//         // Return all unique triplets
+//         return res;
 //     }
-
 // }
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length-2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            // int sum = -1 * a[i]; // ye line nhi likhenge qk hame sum = 0 chahiye /// if likhte then har jagah total to sum se compare karte na ki 0 se 
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-
-                if (total == 0 ){
-
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));/////// important line
-
-                    left++;
-                    right--;
-                    while(left< nums.length && nums[left] == nums[left]-1){
-                        left++;
-                    }
-                    while(right >=0 && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                }
-                else if (total < 0) {
-                    left++;
-                } else {
-                   right--;
-                }
-            }
-        }
-        return res;        
-    }
-}
