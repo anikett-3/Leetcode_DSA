6e00ea4e822771e013a2a7da8276d4550e39f5bd
Aniket kumar <aniket3pd@gmail.com>
2026-09-25 22:10:22 +0530

    Time: 0 ms (100%), Space: 43.6 MB (39.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
         int start = 0;
-        int left= 0;
-        int right = nums.length -1;
-        while(left<=right){
-            if( nums[left] == 2){
-                // swap nums[left], nums[right]
-                int temp = nums[left];
-                nums [left] = nums[right];
-                nums[right] = temp;
-                right --;
+        int mid= 0;
+        int end = nums.length -1;
+        while(mid<=end){
+            if( nums[mid] == 2){
+                // swap nums[mid], nums[end]
+                int temp = nums[mid];
+                nums [mid] = nums[end];
+                nums[end] = temp;
+                end --;
             }
-            else if (nums[left] == 0){
-                // swap nums[left], nums[start]
-                int temp = nums[left];
-                nums[left]= nums[start];
+            else if (nums[mid] == 0){
+                // swap nums[mid], nums[start]
+                int temp = nums[mid];
+                nums[mid]= nums[start];
                 nums[start] = temp;
                 start ++;
-                left ++;
+                mid ++;
             }
             else {
-                left++;
+                mid++;
             }
                 
             
@@ -29,3 +29,23 @@ class Solution {
     }
 }
 
+
+// class Solution {
+//     public void sortColors(int [] nums){
+//         int low = 0,   MID = 0,    high = nums.length-1;
+//         while(mid<=high){
+//             // we suppose -> --> - infinity to low yani --> -infinity to index 0 ===> 0 region
+//             //              -->  + infinity to high + 1  == > 2 region 
+//             // and          --> whole array is == 1 ka region yani --- "MID"
+//             // hamne pura array from index 0  to index 5 tak {mid} manna hai 
+//             // so we compare every element to MID
+//            if( nums[MID] == 2){
+//                 // swap nums[mid], nums[end] /// ////
+//                 int temp = nums[mid];
+//                 nums [mid] = nums[end];
+//                 nums[end] = temp;
+//                 end --;
+//         }
+
+//     }
+// }
